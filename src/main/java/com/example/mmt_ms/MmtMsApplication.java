4d850f5af77747d932061aa066b2c86cf89acc2b
Sanjeev515 <simplesanjeev51@gmail.com>
2024-10-04 22:54:17 +0530

    adding flight mode

---
+++ src/main/java/com/example/mmt_ms/MmtMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mmt_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmtMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmtMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mmt_ms/MmtMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mmt_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmtMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
